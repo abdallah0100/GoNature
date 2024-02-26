0d41c55605f6919d263b0d5f0ae4ee193ed27482
Abdallah <abdlaa1912@gmail.com>
2024-02-26 19:26:49 +0200

    Added client startup frame

--- Client/src/main/ClientUI.java
+++ Client/src/main/ClientUI.java
@@ -2,7 +2,7 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import main.gui.ClientFrameController;
+import main.gui.ClientStartUpController;
 
 public class ClientUI extends Application{
 	
@@ -14,11 +14,10 @@ public class ClientUI extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		//setting up a connection with the server
-		clientController = new ClientController("localhost", 5555);
-		//load the client frame
-		ClientFrameController frameController = new ClientFrameController();
-		frameController.start(primaryStage);
+		//load the client startup frame
+		ClientStartUpController startupC = new ClientStartUpController();
+		startupC.start(primaryStage);
+
 		
 	}
 
