16d4c79e6ba025a104f580c17ba3bbc3001986b0
Abdallah <abdlaa1912@gmail.com>
2024-02-26 18:03:25 +0200

    moving projects

---
+++ Server/src/main/ServerUI.java
@@ -0,0 +1,14 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ServerUI extends Application	{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
