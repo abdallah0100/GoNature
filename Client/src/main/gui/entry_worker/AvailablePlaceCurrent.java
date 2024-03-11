59e7ff1ae4afb42c2c12afdc01c7ac9813d6e3a1
MbadaShehade <mbadaShehade@gmail.com>
2024-03-11 10:39:44 +0200

    Added implementation for EntryWorkerFrame.
    Changed AvailablePlaceCurrent Constructor.

--- Client/src/main/gui/entry_worker/AvailablePlaceCurrent.java
+++ Client/src/main/gui/entry_worker/AvailablePlaceCurrent.java
@@ -8,30 +8,27 @@ public class AvailablePlaceCurrent  {
 	private String parkname;
 	private String date;
 	private String time;
+	
 
-public AvailablePlaceCurrent(String name,String time,String date) {
-	this.parkname=parkname;
-	this.time=time;
-	this.date=date;
-
-}
-
-
-
-public String getnamePark() {
-	return parkname;
-}
-
-public String gettime() {
-	return time;
-}
- 
-public String getdate() {
-	return date;
-}
-
-
-
+	public AvailablePlaceCurrent(String parkname,String time,String date) {
+		this.parkname=parkname;
+		this.time=time;
+		this.date=date;
+	
+	}
+	
+	
+	public String getnamePark() {
+		return parkname;
+	}
+	
+	public String gettime() {
+		return time;
+	}
+	 
+	public String getdate() {
+		return date;
+	}
 
 
 }
--- Client/src/main/gui/entry_worker/EntryWorkerFrameController.java
+++ Client/src/main/gui/entry_worker/EntryWorkerFrameController.java
@@ -4,12 +4,17 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ * This class serves as the controller for the EntryWorkerFrame.fxml file.
+ * It manages the actions and events related to the Entry Worker's interface.
+*/
 
 public class EntryWorkerFrameController extends Application{
 	
@@ -24,6 +29,11 @@ public class EntryWorkerFrameController extends Application{
 		 launch(args);
 	 }
 	 
+	 
+	/**
+    * @param primaryStage the primary stage for the application
+	* @throws Exception if an error occurs during initialization
+    */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = new Pane();
@@ -45,12 +55,44 @@ public class EntryWorkerFrameController extends Application{
 	
 	
 	
-	public void checkAvailablePlaces(ActionEvent e) {
-		//TODO
+	/***
+	 * This method responsible for hiding EntryWorker window
+	 * after clicking on checkAvailablePlaces button and
+	 * new window is opened for the Available Places that the 
+	 * EntryWorker can check.
+	 * @param event
+	 */
+	public void checkAvailablePlaces(ActionEvent event) {
+		((Node)event.getSource()).getScene().getWindow().hide(); //hide EntryWorker window
+		AvailablePlacesFrameController availablePlaceFrame = new AvailablePlacesFrameController();
+		try {
+			availablePlaceFrame.start(new Stage());;
+		}
+		catch (Exception ex) {
+		System.out.println("[availablePlaceFrameController] - Error starting availablePlaceFrame");
+		ex.printStackTrace();
+		}
 	}
 	
-	public void viewBill(ActionEvent e) {
-		//TODO
+	
+	/***
+	 * This method responsible for hiding EntryWorker window
+	 * after clicking on View Bill button and
+	 * new window is opened to check the Bill of the visitor 
+	 * that the EntryWorker can check.
+	 * @param event
+	 */
+	public void viewBill(ActionEvent event) {
+		
+		((Node)event.getSource()).getScene().getWindow().hide(); //hide EntryWorker window
+		BillCakFrameController BillCakFrame = new BillCakFrameController();
+		try {
+			BillCakFrame.start(new Stage());
+		}
+		catch (Exception ex) {
+		System.out.println("[BillCakFrameController] - Error starting BillCakFrame");
+		ex.printStackTrace();
+		}
 	}
 
 }
