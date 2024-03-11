6bb706c6b9e83a1e9f3c3f526e635037eed72d9f
Abdallah Aburomi <abdlaa1912@gmail.com>
2024-03-12 00:28:55 +0200

    removed unused import

--- Client/src/main/gui/LogInFrameController.java
+++ Client/src/main/gui/LogInFrameController.java
@@ -6,12 +6,8 @@ import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import utilities.SceneController;
 
