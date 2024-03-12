f770eb2e6485c882641a8297ac9d2fc005239d34
MbadaShehade <mbadaShehade@gmail.com>
2024-03-12 17:21:00 +0200

    Added design for DepManagerChangeRequestTableFrame.

--- Client/src/main/gui/dep_manager/DepManagerChangeRequestTableFrameController.java
+++ Client/src/main/gui/dep_manager/DepManagerChangeRequestTableFrameController.java
@@ -5,10 +5,19 @@ package main.gui.dep_manager;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import utilities.SceneController;
 
 public class DepManagerChangeRequestTableFrameController extends Application {
+	
+	@FXML
+	private AnchorPane sp;
+	
+	
+		
+	
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
