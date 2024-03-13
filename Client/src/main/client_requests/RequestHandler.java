2e96db2f5ad8f2f6670c2f6c588c9d776fe1de6c
User <User@DESKTOP-8P1AT4Q>
2024-03-13 03:55:00 +0200

    .

--- Client/src/main/client_requests/RequestHandler.java
+++ Client/src/main/client_requests/RequestHandler.java
@@ -10,36 +10,36 @@ import requests.Message;
 public class RequestHandler {
 	
 	public static void handleIncomingRequests(Message msg) {
-		switch (msg.getRequestEnumType()) {
-	    case REQUEST_ERROR:
-	        System.out.println("[RequestHandler] - Server responded with an error: " + msg.getRequestData());
-	        break;
-	    case GENERAL_RESPOND:
-	        System.out.println("[RequestHandler] - ServerResponse: " + msg.getRequestData());
-	        break;
-	    case VALIDATE_VISITOR:
-	        if (msg.getRequestData() instanceof Visitor)
-	            ClientController.connectedVisitor = (Visitor) msg.getRequestData();
-	        else {
-	            System.out.println("[RequestHandler] - invalid VALIDATE_VISITOR response");
-	            return;
-	        }
-	        VisitorRequestController.finishedValidating = true;
-	        break;
-	    case LOGIN_USER:
-	        if (msg.getRequestData() instanceof User)
-	            ClientController.connectedUser = (User) msg.getRequestData();
-	        else {
-	            System.out.println("[RequestHandler] - invalid LOGIN_USER response");
-	            return;
-	        }
-	        UserRequestController.LogedIn = true;
-	        break;
-	    default:
-	        System.out.println("[RequestHandler] - unimplemented message type: " + msg.toString());
-	        if (msg.getRequestData() != null)
-	            System.out.println("[RequestHandler] - Received data: " + msg.getRequestData());
-	        break;
+		switch(msg.getRequestEnumType()) {
+		case REQUEST_ERROR:
+			System.out.println("[GoNatureClient] - Server responded with an error: " + msg.getRequestData());
+			break;
+		case GENERAL_RESPOND:
+			System.out.println("[GoNatureClient] - ServerResponse: " + msg.getRequestData());
+			break;
+			default:
+				System.out.println("[GoNatureClient] - unimplemented message type: " + msg.toString());
+				if (msg.getRequestData() != null)
+					System.out.println("[GoNatureClient] - Received data: " + msg.getRequestData());
+				break;
+			case VALIDATE_VISITOR:
+				if (msg.getRequestData() instanceof Visitor)
+					ClientController.connectedVisitor = (Visitor) msg.getRequestData();
+				else {
+					System.out.println("[RequestHandler] - invalid VALIDATE_VISITOR response");
+					return;
+				}
+				VisitorRequestController.finishedValidating = true;
+				break;
+			case LOGIN_USER:
+				if (msg.getRequestData() instanceof User)
+					ClientController.connectedUser = (User) msg.getRequestData();
+				else {
+					System.out.println("[RequestHandler] - invalid LOGIN_USER response");
+					return;
+				}
+				UserRequestController.LogedIn = true;
+				break;
 				
 		}
 	}
