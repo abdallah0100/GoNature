92c87ff8cb8bb18c5519b8e87439648123e681bd
Abdallah Aburomi <abdlaa1912@gmail.com>
2024-03-14 01:48:58 +0200

    renamed a few files

---
+++ Client/src/main/gui/park_manager/ParkManagerSidePaneController.java
@@ -0,0 +1,21 @@
+package main.gui.park_manager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+public class ParkManagerSidePaneController {
+
+	@FXML
+	private Button preparereport;
+	
+	@FXML
+	private Button editparkvarible;
+	
+		
+		
+	public void preparereport() {	
+		
+	}
+	
+	public void editparkvarible() {
+		
+	}
+}
