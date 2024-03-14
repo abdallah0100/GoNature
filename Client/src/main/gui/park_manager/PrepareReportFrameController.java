f4e5993664a7b40e7f9d71b65da799c1da63e7f0
Abdallah Aburomi <abdlaa1912@gmail.com>
2024-03-14 22:05:22 +0200

    interface edits to prepare report

--- Client/src/main/gui/park_manager/PrepareReportFrameController.java
+++ Client/src/main/gui/park_manager/PrepareReportFrameController.java
@@ -1,13 +1,16 @@
 package main.gui.park_manager;
 
 import java.net.URL;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -17,11 +20,7 @@ public class PrepareReportFrameController implements Initializable{
 	@FXML
 	private Text reportType;
 	@FXML
-	private DatePicker dateField;
-	@FXML
 	private Pane rightPane;
-	@FXML
-	private TextField nameField;
 	@FXML
 	private TextField parkField;
 	@FXML
@@ -32,10 +31,27 @@ public class PrepareReportFrameController implements Initializable{
 	private Button generateBtn;
 	@FXML
 	private Button cancelBtn;
+	@FXML
+	private ComboBox<String> monthBox;
+	@FXML
+	private ComboBox<String> yearBox;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		String[] monthsArray = {"January", "February", "March", "April",
+							"May", "June", "july", "August",
+							"September", "October", "November", "December"};
+		ObservableList<String> months = FXCollections.observableArrayList(monthsArray);		
+		monthBox.setItems(months);
 		
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		String[] years = new String[3];
+		years[0] = year + "";
+		years[1] = (year -1) + "";
+		years[2] = (year -2) + "";
+		
+		ObservableList<String> yearList = FXCollections.observableArrayList(years);
+		yearBox.setItems(yearList);
 	}
 	
 	@FXML
@@ -71,8 +87,6 @@ public class PrepareReportFrameController implements Initializable{
 		generateBtn.setVisible(false);
 		
 		parkField.setText("");
-		nameField.setText("");
-		dateField.setValue(null);
 	}
 
 }
