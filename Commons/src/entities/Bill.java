88cc5fa4f78da6397686c3808e12b5c955bf2d37
User <User@DESKTOP-8P1AT4Q>
2024-03-16 02:30:40 +0200

    .

--- Commons/src/entities/Bill.java
+++ Commons/src/entities/Bill.java
@@ -21,7 +21,7 @@ public class Bill implements Serializable {
 		this.type1=type1;
 		this.invited=invisted;
 		this.payed=payed;
-	}
+	} 
 	 public void print() {
 		 System.out.println(getType());
 		 System.out.println(getNumberOfVisitor());
@@ -67,7 +67,7 @@ public class Bill implements Serializable {
 	
 	public double returnPrice() {
 		//1 מראש  ביקור אישי 
-		if(getType().equals("Individual")) 
+		if(getType().equals("Private")) 
 		{
 			
 			if(getinvited() &&getPayed()) {
