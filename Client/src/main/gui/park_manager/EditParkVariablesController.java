275e493108f12c51269dbb122ca95ba3e390496b
Abdallah Aburomi <abdlaa1912@gmail.com>
2024-03-17 15:36:06 +0200

    update park variables
    Updated park variable interface and added basic loading

--- Client/src/main/gui/park_manager/EditParkVariablesController.java
+++ Client/src/main/gui/park_manager/EditParkVariablesController.java
@@ -1,43 +1,62 @@
 package main.gui.park_manager;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import main.ClientController;
 
-public class EditParkVariablesController{
+public class EditParkVariablesController implements Initializable{
 	 	
 	@FXML
-	private Button EditEstimatedTime;
-
+	private Button editEstimatedTimeBtn;
 	@FXML
-	private Button EditVisitor;
-
+	private Button editVisitorBtn;
 	@FXML
-	private Button edit;
-
+	private Button editGapBtn;
 	@FXML
-	private TextField oldValue;
-
+	private Pane rightPane;
+	@FXML
+	private Label editingVariable;
+	@FXML
+	private Label parkName;
+	@FXML
+	private Label msgLabel;
+	@FXML
+	private Label currentValue;
+	@FXML
+	private Button update;
+	@FXML
+	private Button cancel;
+	
 	@FXML
 	private TextField newValue;
-
+		    
 	@FXML
-	private Button editGap;
-		    
-		    
-	public void editEstimatedTime(ActionEvent e){
+	public void selectVariableToEdit(ActionEvent e ) {
+	    Button clickedBtn = (Button)e.getSource();
+	    editingVariable.setText(clickedBtn.getText());
+	    rightPane.setVisible(true);
+	}
+	@FXML
+	public void cancel(ActionEvent e ) {
+		newValue.setText("");
+		rightPane.setVisible(false);
+	}
+	@FXML
+	public void update(ActionEvent e ) {
+	    	
+	}
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		parkName.setText(ClientController.connectedUser.getParkWork());
 		
 	}
 		    
-	public void editGap(ActionEvent e ) {   
-		
-	}
-	
-	public void edit(ActionEvent e ) {
-	    	
-	}
-		    
-	public void editVisitor(ActionEvent e) {
-	    	
-	}
+
 }
