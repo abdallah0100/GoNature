c0eaab1eb171afa09305f50d42c87ae9a69ef0c5
User <User@DESKTOP-BJK97OG>
2024-03-26 17:26:32 +0200

    documentation
    finished documentation for the package entities in Commons.

--- Commons/src/entities/InboxMessage.java
+++ Commons/src/entities/InboxMessage.java
@@ -2,55 +2,95 @@ package entities;
 
 import java.io.Serializable;
 
+/**
+ * Class InboxMessage saves all data related to a message sent to a visitor.
+ * The message is sent when the visitor makes a reservation or when he enters a waiting list.
+ * The class contains a couple of message contents like title, the content the id and if it was deleted.
+ */
 public class InboxMessage implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private String title;
-	private String content;
-	private int id;
-	private boolean deleted;
+	private String title; // The message title.
+	private String content; // The message content.
+	private int id; // The message id.
+	private boolean deleted; // Flag indicating if the message is deleted. 
 	
+	/**
+	 * Constructs a new InboxMessage with the id, title and content.
+	 * @param id
+	 * @param title
+	 * @param content
+	 */
 	public InboxMessage(int id, String title, String content) {
 		this.id = id;
 		this.title = title;
 		this.content = content;
 	}
 	
+	/**
+	 * Constructs a new InboxMessage with the message title and content. 
+	 * @param title
+	 * @param content
+	 */
 	public InboxMessage(String title, String content) {
 		this.title = title;
 		this.content = content;
 	}
 	
+	/**
+	 * @return The message title.
+	 */
 	public String getTitle() {
 		return title;
 	}
+	/**
+	 * Sets the message title.
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	/**
+	 * @return The message content.
+	 */
 	public String getContent() {
 		return content;
 	}
+	/**
+	 * Sets the message content.
+	 * @param content
+	 */
 	public void setContent(String content) {
 		this.content = content;
 	}
 
+	/**
+	 * @return The message id.
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the message id.
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return True if the message is deleted false otherwise. 
+	 */
 	public boolean isDeleted() {
 		return deleted;
 	}
 
+	/**
+	 * Sets true if the message is deleted false otherwise.
+	 * @param deleted
+	 */
 	public void setDeleted(boolean deleted) {
 		this.deleted = deleted;
-	}
-	
+	}	
 }
--- Commons/src/entities/Visitor.java
+++ Commons/src/entities/Visitor.java
@@ -2,19 +2,27 @@ package entities;
 
 import java.io.Serializable;
 
+/**
+ * Represents a visitor within a park system.
+ * The class tracks visitor information including identification, contact details, and roles.
+ */
 public class Visitor implements Serializable{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	private String id;
-	private String email;
-	private String phone;
-	private String name;
-	private boolean isInstructor;
+	private String id; // The id of the visitor , he login into the system using his id.
+	private String email; // The email of the visitor.
+	private String phone; // The phone number of the visitor so the system can sent messages to the visitor.
+	private String name; // The name of the visitor.
+	private boolean isInstructor; // Flag indicating whether the visitor is an instructor.
+	private boolean foundInDB; // Flag indicating whether the visitor is in the DB.
 	
-	private boolean foundInDB;
-	
+	/**
+	 * Constructs a new Visitor with specified ID, email, phone number, and instructor status.
+	 * @param id
+	 * @param email
+	 * @param phone
+	 * @param instructor
+	 */
 	public Visitor(String id, String email, String phone, boolean instructor) {
 		this.setId(id);
 		this.setEmail(email);
@@ -22,6 +30,13 @@ public class Visitor implements Serializable{
 		this.setInstructor(instructor);
 	}
 	
+	/**
+	 * Constructs a new Visitor with specified ID, email, phone number, and name.
+	 * @param id
+	 * @param email
+	 * @param phone
+	 * @param name
+	 */
 	public Visitor(String id, String email, String phone, String name) {
 		this.setId(id);
 		this.setName(name);
@@ -29,54 +44,101 @@ public class Visitor implements Serializable{
 		this.setPhone(phone);
 
 	}
+	/**
+	 * @return The name of the visitor.
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * Sets the name of the visitor.
+	 * @param Name
+	 */
 	public void setName(String Name) {
 		this.name = Name;
 	}
 
+	/**
+	 * Sets the id of the visitor.
+	 * And sets if found in DB.
+	 * @param id 
+	 */
 	public Visitor (String id) {
 		this.id = id;
 		this.foundInDB = false;
 	}
 
+	/**
+	 * @return the id of the visitor.
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the id of the visitor.
+	 * @param id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return The email of the visitor.
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Sets the email of the visitor.
+	 * @param email
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * @return The phone number of the visitor so the system can sent messages to the visitor.
+	 */
 	public String getPhone() {
 		return phone;
 	}
 
+	/**
+	 * Sets the phone number of the visitor so the system can sent messages to the visitor.
+	 * @param phone
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+	/**
+	 * @return True if the visitor is an instructor false otherwise.
+	 */
 	public boolean isInstructor() {
 		return isInstructor;
 	}
 
+	/**
+	 * Sets true if the visitor is an instructor false otherwise.
+	 * @param isInstructor
+	 */
 	public void setInstructor(boolean isInstructor) {
 		this.isInstructor = isInstructor;
 	}
 
+	/**
+	 * @return True if the visitor is in DB false otherwise.
+	 */
 	public boolean isFoundInDB() {
 		return foundInDB;
 	}
 
+	/**
+	 * Sets true if the visitor is found in DB false otherwise.
+	 * @param foundInDB
+	 */
 	public void setFoundInDB(boolean foundInDB) {
 		this.foundInDB = foundInDB;
 	}
